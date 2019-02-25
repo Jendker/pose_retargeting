d273355f85bb39773b25927031b2e4b5880d9804
Jedrzej Beniamin Orbik <Jendker@users.noreply.github.com>
2019-02-25 15:50:56 +0100

    Added pose transformation

--- scripts/mapper.py
+++ scripts/mapper.py
@@ -2,6 +2,9 @@
 
 import vrep
 import numpy as np
+from numpy.linalg import inv
+import sched, time
+import math
 
 
 class Mapper:
@@ -10,26 +13,177 @@ class Mapper:
         self.clientID = vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5)  # Connect to V-REP
         if self.clientID != -1:
             print('Connected to remote API server')
+        self.K_matrix = np.identity(3)
+        self.human_hand_vel = np.zeros(3)
+        self.sampling_time = 0.03  # in seconds
+        _, self.finger_tip_handle = vrep.simxGetObjectHandle(self.clientID, 'ITIP_tip', vrep.simx_opmode_blocking)
+        _, self.IDIP_joint_handle = vrep.simxGetObjectHandle(self.clientID, 'IDIP_joint', vrep.simx_opmode_blocking)
+        _, self.IPIP_joint_handle = vrep.simxGetObjectHandle(self.clientID, 'IPIP_joint', vrep.simx_opmode_blocking)
+        _, self.IMCP_front_joint_handle = vrep.simxGetObjectHandle(self.clientID, 'IMCP_front_joint',
+                                                                   vrep.simx_opmode_blocking)
+        _, self.IMCP_side_joint_handle = vrep.simxGetObjectHandle(self.clientID, 'IMCP_side_joint',
+                                                                  vrep.simx_opmode_blocking)
+        self.last_human_hand_pose = self.__initializeHumanHandPose(self.finger_tip_handle)
+        self.last_callback_time = 0
+        self.weight_matrix_inv = np.identity(3)
+        self.damping_matrix = np.identity(3) * 0.1
+        self.last_data = []
 
-    def mapFinger(self, finger_pose):
-        self.__getJacobian()
-        for pose in finger_pose:
-            pass
+    def __initializeHumanHandPose(self, handle):
+        _, current_pos = vrep.simxGetObjectPosition(self.clientID, handle, -1,
+                                                    vrep.simx_opmode_blocking)
+        return current_pos
+
+    def __setJointsTargetVelocity(self, joints_velocities):
+        list_joints_handles = [self.IDIP_joint_handle, self.IPIP_joint_handle, self.IMCP_front_joint_handle,
+                               self.IMCP_side_joint_handle]
+        for index, velocity in enumerate(joints_velocities):
+            vrep.simxSetJointTargetVelocity(self.clientID, list_joints_handles[index], velocity,
+                                            vrep.simx_opmode_oneshot)
+
+    def __getError(self):
+        _, current_pos = vrep.simxGetObjectPosition(self.clientID, self.finger_tip_handle, -1,
+                                                    vrep.simx_opmode_blocking)
+        current_pos_vec = np.array(current_pos)
+        return self.last_human_hand_pose - current_pos_vec
+
+    def __getPseudoInverseJacobian(self):
+        jacobian = self.__getJacobian()
+        return np.linalg.multi_dot([self.weight_matrix_inv, jacobian.T, inv(
+            np.linalg.multi_dot([jacobian, self.weight_matrix_inv, jacobian.T]) + self.damping_matrix)])
+
+    def __setFingerTarget(self, scheduler):
+        error = self.__getError()
+        pseudo_inverse_jacobian = self.__getPseudoInverseJacobian()
+        q_vel = np.dot(pseudo_inverse_jacobian, (self.human_hand_vel + np.dot(self.K_matrix, error)))
+        self.__setJointsTargetVelocity(q_vel)
+        # continue with loop
+        scheduler.enter(self.sampling_time, 1, self.__setFingerTarget, (scheduler,))
 
     def __getJacobian(self):
         empty_buff = bytearray()
         _, dimension, jacobian_vect, _, _ = vrep.simxCallScriptFunction(self.clientID, 'remoteApiCommandServer',
-                                                                  vrep.sim_scripttype_childscript,
-                                                                  'jacobianIKGroup', [], [],
-                                                                  ['IK_Group'], empty_buff,
-                                                                  vrep.simx_opmode_blocking)
-        jacobian = np.mat(jacobian_vect).reshape(dimension)
-        print(dimension)
-        print(jacobian)
+                                                                        vrep.sim_scripttype_childscript,
+                                                                        'jacobianIKGroup', [], [],
+                                                                        ['IK_Group'], empty_buff,
+                                                                        vrep.simx_opmode_blocking)
+        jacobian = np.array(jacobian_vect).reshape(dimension)
+        return jacobian
+
+    def __rodriguezToRotationMatrix(self, axis, theta):
+        """
+        Return the rotation matrix associated with counterclockwise rotation about
+        the given axis by theta radians.
+        """
+        axis = np.asarray(axis)
+        axis = axis / np.linalg.norm(axis)
+        a = math.cos(theta / 2.0)
+        b, c, d = -axis * math.sin(theta / 2.0)
+        aa, bb, cc, dd = a * a, b * b, c * c, d * d
+        bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+        return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                         [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                         [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
+
+    def skew(self, vector):
+        """
+        this function returns a numpy array with the skew symmetric cross product matrix for vector.
+        the skew symmetric cross product matrix is defined such that
+        np.cross(a, b) = np.dot(skew(a), b)
+
+        :param vector: An array like vector to create the skew symmetric cross product matrix for
+        :return: A numpy array of the skew symmetric cross product vector
+        """
+
+        return np.array([[0, -vector[2], vector[1]],
+                        [vector[2], 0, -vector[0]],
+                        [-vector[1], vector[0], 0]])
+
+    def __eulerAnglesToRotationMatrix(self, theta):
+
+        R_x = np.array([[1, 0, 0],
+                        [0, math.cos(theta[0]), -math.sin(theta[0])],
+                        [0, math.sin(theta[0]), math.cos(theta[0])]
+                        ])
+
+        R_y = np.array([[math.cos(theta[1]), 0, math.sin(theta[1])],
+                        [0, 1, 0],
+                        [-math.sin(theta[1]), 0, math.cos(theta[1])]
+                        ])
+
+        R_z = np.array([[math.cos(theta[2]), -math.sin(theta[2]), 0],
+                        [math.sin(theta[2]), math.cos(theta[2]), 0],
+                        [0, 0, 1]
+                        ])
+
+        R = np.dot(R_z, np.dot(R_y, R_x))
+        return R
+
+    def __transformationVector(self, from_point, to_point):
+        return to_point - from_point
+
+    def __getRotationMatrixFromVectors(self, desired_vector, given_vector):
+        desired_vector = desired_vector / np.linalg.norm(desired_vector)
+        given_vector = given_vector / np.linalg.norm(given_vector)
+        rotation_axis = np.cross(desired_vector, given_vector)
+        c = np.dot(given_vector, desired_vector)
+        if c == -1:
+            return np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]])
+
+        skew_mat = self.skew(rotation_axis)
+        # https://math.stackexchange.com/questions/180418/calculate-rotation-matrix-to-align-vector-a-to-vector-b-in-3d
+        rotation_matrix = np.identity(3) + skew_mat + np.linalg.multi_dot([skew_mat, skew_mat, (1.0/1.0+c)])
+        return rotation_matrix
+
+    def __euclideanTransformation(self, rotationMatrix, transformationVector):
+        top = np.concatenate((rotationMatrix, transformationVector[:, np.newaxis]), axis=1)
+        return np.concatenate((top, np.array([0, 0, 0, 1])[np.newaxis, :]), axis=0)
+
+    def __transformHandToOrigin(self, data):
+        position_palm_base = self.__getPositionVectorForDataIndex(data, 0)
+        position_knuckle_middle_finger = self.__getPositionVectorForDataIndex(data, 3)
+        position_knuckle_index_finger = self.__getPositionVectorForDataIndex(data, 2)
+        given_vector = position_knuckle_middle_finger - position_palm_base
+        target_vector = np.array([0, 0, 1])
+        given_vector = np.array([0, 0, -1])
+        rotation_matrix = self.__getRotationMatrixFromVectors(desired_vector=target_vector, given_vector=given_vector)
+        _, sim_list_position_knuckle_index_finger = vrep.simxGetObjectPosition(self.clientID, self.finger_tip_handle, -1,
+                                                    vrep.simx_opmode_blocking)
+        transformation_vector = self.__transformationVector(position_knuckle_index_finger,
+                                                            np.array(sim_list_position_knuckle_index_finger))
+        euclidean_transformation_matrix = self.__euclideanTransformation(rotation_matrix, transformation_vector)
+        # print (euclidean_transformation_matrix)
+        new_data = []
+        for point in self.__dataToPointsList(data):
+            extended_point = np.concatenate((point, [1]))
+            new_data.append(np.dot(euclidean_transformation_matrix, extended_point)[0:3])
+        return new_data
+
+    def __getPositionVectorForDataIndex(self, data, index):
+        joint = data.joints_position[index]
+        position = [joint.x, joint.y, joint.z]
+        return np.array(position)
+
+    def __dataToPointsList(self, data):
+        pointList = []
+        for point in data.joints_position:
+            this_point = [point.x, point.y, point.z]
+            pointList.append(np.array(this_point))
+        return pointList
 
     def callback(self, data):
-        finger_indices = [5, 6, 7, 8]
-        finger_pose = []
-        for index in finger_indices:
-            finger_pose.append(data.joints_position[index])
-        self.mapFinger(finger_pose)
+        current_time = time.time()
+        self.last_data = self.__transformHandToOrigin(data)
+        HPE_finger_tip_pose = self.last_data[11]
+        if self.last_callback_time != 0:
+            self.human_hand_vel = (HPE_finger_tip_pose - self.last_human_hand_pose) / current_time
+            self.last_callback_time = current_time
+        else:
+            self.last_callback_time = current_time
+        self.last_human_hand_pose = HPE_finger_tip_pose
+        print(HPE_finger_tip_pose)
+
+    def execute(self):
+        scheduler = sched.scheduler(time.time, time.sleep)
+        scheduler.enter(self.sampling_time, 1, self.__setFingerTarget, (scheduler,))
+        scheduler.run()
