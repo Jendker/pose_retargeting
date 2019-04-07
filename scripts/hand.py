53436e23adfbd1d6838fb3bc171afa7871334851
Jedrzej Beniamin Orbik <Jendker@users.noreply.github.com>
2019-04-07 14:32:58 +0200

    Fixed transformation matrices

--- scripts/hand.py
+++ scripts/hand.py
@@ -191,21 +191,20 @@ class Hand:
         index_finger = HandPart(self.clientID, ['IMCP_side_joint', 'IMCP_front_joint', 'IPIP_joint', 'IDIP_joint'], 'ITIP_tip',
                                 [['ITIP_tip', 'IPIP_joint'], ['IMCP_side_joint', 'IMCP_side_joint'], [11, 9]],
                                 [[10., -10.], [100., 0.], [90., 0.], [90., 0.]], ConfigurationType.finger)
-        # middle_finger = HandPart(self.clientID, ['MMCP_side_joint', 'MMCP_front_joint', 'MPIP_joint', 'MDIP_joint'], 'MTIP_tip',
-        #                         [['MTIP_tip', 'MPIP_joint'], ['MMCP_side_joint', 'MMCP_side_joint'], [14, 12]],
-        #                         [[10., -10.], [100., 0.], [90., 0.], [90., 0.]], ConfigurationType.finger)
-        # ring_finger = HandPart(self.clientID, ['RMCP_side_joint', 'RMCP_front_joint', 'RPIP_joint', 'RDIP_joint'], 'RTIP_tip',
-        #                         [['RTIP_tip', 'RPIP_joint'], ['RMCP_side_joint', 'RMCP_side_joint'], [17, 15]],
-        #                         [[10., -10.], [100., 0.], [90., 0.], [90., 0.]], ConfigurationType.finger)
-        # pinkie_finger = HandPart(self.clientID, ['PMCP_side_joint', 'PMCP_front_joint', 'PPIP_joint', 'PDIP_joint'], 'PTIP_tip',
-        #                         [['PTIP_tip', 'PPIP_joint'], ['PMCP_side_joint', 'PMCP_side_joint'], [20, 18]],
-        #                         [[10., -10.], [100., 0.], [90., 0.], [90., 0.]], ConfigurationType.finger)
-        # thumb_finger = HandPart(self.clientID, ['TMCP_rotation_joint', 'TMCP_front_joint', 'TPIP_side_joint', 'TPIP_front_joint', 'TDIP_joint'], 'TTIP_tip',
-        #                         [['TTIP_tip', 'TPIP_front_joint'], ['TMCP_rotation_joint', 'TMCP_rotation_joint'], [8, 6]],
-        #                         [[60., -60.], [70., 0.], [30., -30.], [12., -12.], [90, 0]], ConfigurationType.thumb)
+        middle_finger = HandPart(self.clientID, ['MMCP_side_joint', 'MMCP_front_joint', 'MPIP_joint', 'MDIP_joint'], 'MTIP_tip',
+                                [['MTIP_tip', 'MPIP_joint'], ['MMCP_side_joint', 'MMCP_side_joint'], [14, 12]],
+                                [[10., -10.], [100., 0.], [90., 0.], [90., 0.]], ConfigurationType.finger)
+        ring_finger = HandPart(self.clientID, ['RMCP_side_joint', 'RMCP_front_joint', 'RPIP_joint', 'RDIP_joint'], 'RTIP_tip',
+                                [['RTIP_tip', 'RPIP_joint'], ['RMCP_side_joint', 'RMCP_side_joint'], [17, 15]],
+                                [[10., -10.], [100., 0.], [90., 0.], [90., 0.]], ConfigurationType.finger)
+        pinkie_finger = HandPart(self.clientID, ['PMCP_side_joint', 'PMCP_front_joint', 'PPIP_joint', 'PDIP_joint'], 'PTIP_tip',
+                                [['PTIP_tip', 'PPIP_joint'], ['PMCP_side_joint', 'PMCP_side_joint'], [20, 18]],
+                                [[10., -10.], [100., 0.], [90., 0.], [90., 0.]], ConfigurationType.finger)
+        thumb_finger = HandPart(self.clientID, ['TMCP_rotation_joint', 'TMCP_front_joint', 'TPIP_side_joint', 'TPIP_front_joint', 'TDIP_joint'], 'TTIP_tip',
+                                [['TTIP_tip', 'TPIP_front_joint'], ['TMCP_rotation_joint', 'TMCP_rotation_joint'], [8, 6]],
+                                [[60., -60.], [70., 0.], [30., -30.], [12., -12.], [90, 0]], ConfigurationType.thumb)
 
-        self.hand_parts_list = [index_finger]
-        # self.hand_parts_list = [thumb_finger]
+        self.hand_parts_list = (index_finger, middle_finger, ring_finger, pinkie_finger, thumb_finger)
 
     def __del__(self):
         for hand_part in self.hand_parts_list:
--- scripts/jacobian_calculation.py
+++ scripts/jacobian_calculation.py
@@ -6,7 +6,6 @@ import time
 import rospy
 import pickle
 import os
-import math
 
 
 class ConfigurationType(Enum):
@@ -22,35 +21,30 @@ class JacobianCalculation:
         self.all_handles = transformation_handles[:]  # contains also finger tip
         self.q = sp.symbols('q_0:{}'.format(len(self.joint_handles)))
         self.joint_handle_q_map = dict(zip(self.joint_handles, self.q))
-        self.Ts = []
         for joint_handle in self.joint_handles:  # initialize streaming
             result, _ = vrep.simxGetJointPosition(self.clientID, joint_handle, vrep.simx_opmode_streaming)
         while result != vrep.simx_return_ok:
             result, _ = vrep.simxGetJointPosition(self.clientID, self.joint_handles[0], vrep.simx_opmode_buffer)
             time.sleep(0.01)
-        self.f = self.calculateJacobian(configuration_type)
+        self.jacobian, self.Ts = self.calculateJacobian(configuration_type)
 
     def updateClientID(self, clientID):
         self.clientID = clientID
 
     def calculateJacobian(self, configuration_type):
-        jacobians_filename = 'jacobians.dat'
-        jacobian = sp.zeros(len(self.joint_handles), 3 * len(self.task_object_handles_and_bases))
+        if not os.path.isdir("jacobians"):
+            os.mkdir("jacobians")
+        jacobian_configuration = self.task_object_handles_and_bases
+        conf_string = np.array_str(np.array(jacobian_configuration)).replace("\n", "")
+        jacobians_filename = "jacobians/" + conf_string + ".dat"
 
         already_calculated = False
-        jacobian_configuration = tuple(self.task_object_handles_and_bases)
         if os.path.isfile(jacobians_filename):
             with open(jacobians_filename, 'rb') as handle:
-                jacobians_dict = pickle.load(handle)
-
-        else:
-            jacobians_dict = {}
-
-        if jacobian_configuration in jacobians_dict:
-            jacobian_from_dict = jacobians_dict[jacobian_configuration]
-            self.Ts = jacobian_from_dict[0]
-            jacobian = jacobian_from_dict[1]
-            already_calculated = True
+                calculated_configuration = pickle.load(handle)
+                Ts = calculated_configuration[0]
+                jacobian = calculated_configuration[1]
+                already_calculated = True
 
         if not already_calculated:
             objects_positions = {}
@@ -58,6 +52,9 @@ class JacobianCalculation:
                 _, this_object_position = vrep.simxGetObjectPosition(self.clientID, joint_handle, -1,
                                                                      vrep.simx_opmode_blocking)
                 objects_positions[joint_handle] = np.array(this_object_position)
+
+            jacobian = sp.zeros(len(self.joint_handles), 3 * len(self.task_object_handles_and_bases))
+            Ts = []
             if configuration_type == ConfigurationType.finger:
                 for task_index, [target_handle, base_handle] in enumerate(self.task_object_handles_and_bases):
                     transformations = []
@@ -77,11 +74,7 @@ class JacobianCalculation:
                         continue
 
                     for index, joint_handle in enumerate(transformation_handles):
-                        vector_this_object_position = objects_positions[joint_handle]
-
                         if joint_handle == target_handle:
-                            # we need the rotation here for some hand parts
-                            # for now this is fine
                             last_translation = np.linalg.norm(objects_positions[transformation_handles[index]] -
                                                               objects_positions[transformation_handles[index - 1]])
                             transformations.append(sp.Matrix([[1, 0, 0, 0],
@@ -89,8 +82,10 @@ class JacobianCalculation:
                                                               [0, 0, 1, last_translation],
                                                               [0, 0, 0, 1]]))
                             continue
-                        angle = self.joint_handle_q_map[joint_handle]  # -angle, because the joints turn clockwise
+
+                        angle = self.joint_handle_q_map[joint_handle]
                         if index == 0:
+                            vector_this_object_position = objects_positions[joint_handle]
                             transformations.append(sp.Matrix([[1, 0, 0, vector_this_object_position[0]],
                                                               [0, 1, 0, vector_this_object_position[1]],
                                                               [0, 0, 1, vector_this_object_position[2]],
@@ -101,24 +96,26 @@ class JacobianCalculation:
                                                               [0, 0, 0, 1]]))
                             continue
 
-                        length = np.linalg.norm(objects_positions[transformation_handles[index]] - objects_positions[transformation_handles[index - 1]])
+                        length = np.linalg.norm(objects_positions[transformation_handles[index]] - objects_positions[
+                            transformation_handles[index - 1]])
                         # if length < 0.001:
                         #     length = 0.
                         transformations.append(sp.Matrix([[1, 0, 0, 0],
-                                                          [0, sp.cos(angle), -sp.sin(angle), length],
+                                                          [0, sp.cos(angle), -sp.sin(angle), 0],
                                                           [0, sp.sin(angle), sp.cos(angle), length],
                                                           [0, 0, 0, 1]]))
+
                     T = sp.eye(4)
                     for index, transformation in enumerate(transformations):
                         T = T * transformation
                     T = sp.simplify(T)
-                    self.Ts.append(T)
+                    Ts.append(T)
                     this_jacobian = T[0:3, 3].T.jacobian(sp.Matrix(self.q)).T
                     jacobian[:, task_index * 3: task_index * 3 + 3] = this_jacobian
                 jacobian = sp.simplify(jacobian)
-                jacobians_dict[jacobian_configuration] = (self.Ts, jacobian)
-                # with open(jacobians_filename, 'wb') as handle:
-                #     pickle.dump(jacobians_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                calculated_configuration = (Ts, jacobian)
+                with open(jacobians_filename, 'wb') as handle:
+                    pickle.dump(calculated_configuration, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
             elif configuration_type == ConfigurationType.thumb:
                 for task_index, [target_handle, base_handle] in enumerate(self.task_object_handles_and_bases):
@@ -142,8 +139,6 @@ class JacobianCalculation:
                         vector_this_object_position = objects_positions[joint_handle]
 
                         if joint_handle == target_handle:
-                            # we need the rotation here for some hand parts
-                            # for now this is fine
                             last_translation = np.linalg.norm(objects_positions[transformation_handles[index]] -
                                                               objects_positions[transformation_handles[index - 1]])
                             transformations.append(sp.Matrix([[1, 0, 0, 0],
@@ -158,31 +153,34 @@ class JacobianCalculation:
                                                               [0, 1, 0, vector_this_object_position[1]],
                                                               [0, 0, 1, vector_this_object_position[2]],
                                                               [0, 0, 0, 1]]))
-                            deg_to_rad = 45/180 * math.pi
-                            transformations.append(sp.Matrix([sp.cos(deg_to_rad), 0, sp.sin(deg_to_rad), 0],
-                                                             [0, 1, 0, 0],
-                                                             [-sp.sin(deg_to_rad), 0, sp.cos(deg_to_rad), 0],
-                                                             [0, 0, 0, 1]))
-                            transformations.append(sp.Matrix([[math.cos(angle), -math.sin(angle), 0, 0],
-                                                              [math.sin(angle), math.cos(angle), 0, 0],
+                            deg_to_rad = sp.pi / 4.0
+                            transformations.append(sp.Matrix([[sp.cos(deg_to_rad), 0, sp.sin(deg_to_rad), 0],
+                                                              [0, 1, 0, 0],
+                                                              [-sp.sin(deg_to_rad), 0, sp.cos(deg_to_rad), 0],
+                                                              [0, 0, 0, 1]]))
+                            transformations.append(sp.Matrix([[sp.cos(angle), -sp.sin(angle), 0, 0],
+                                                              [sp.sin(angle), sp.cos(angle), 0, 0],
                                                               [0, 0, 1, 0],
                                                               [0, 0, 0, 1]]))
                             continue
 
-                        length = np.linalg.norm(objects_positions[transformation_handles[index]] - objects_positions[transformation_handles[index - 1]])
+                        length = np.linalg.norm(objects_positions[transformation_handles[index]] - objects_positions[
+                            transformation_handles[index - 1]])
                         # if length < 0.001:
                         #     length = 0.
                         if index == 1 or index == 3:
                             transformations.append(sp.Matrix([[1, 0, 0, 0],
-                                                              [0, sp.cos(angle), -sp.sin(angle), sp.sin(angle) * length],
-                                                              [0, sp.sin(angle), sp.cos(angle), sp.cos(angle) * length],
+                                                              [0, sp.cos(angle), -sp.sin(angle), 0],
+                                                              [0, sp.sin(angle), sp.cos(angle), length],
                                                               [0, 0, 0, 1]]))
                             continue
                         if index == 2 or index == 4:
-                            transformations.append(sp.Matrix([[sp.cos(angle), 0, sp.sin(angle), -sp.sin(angle) * length],
-                                                              [0, 1, 0, 0],
-                                                              [-sp.sin(angle), 0, sp.cos(angle), sp.cos(angle) * length],
-                                                              [0, 0, 0, 1]]))
+                            angle = -angle  # these two joints are rotated by 180* in VREP
+                            transformations.append(
+                                sp.Matrix([[sp.cos(angle), 0, sp.sin(angle), 0],
+                                           [0, 1, 0, 0],
+                                           [-sp.sin(angle), 0, sp.cos(angle), length],
+                                           [0, 0, 0, 1]]))
                             continue
                         rospy.logerr("Transformation index not defined")
                         exit(1)
@@ -191,33 +189,35 @@ class JacobianCalculation:
                     for index, transformation in enumerate(transformations):
                         T = T * transformation
                     T = sp.simplify(T)
-                    self.Ts.append(T)
+                    Ts.append(T)
                     this_jacobian = T[0:3, 3].T.jacobian(sp.Matrix(self.q)).T
                     jacobian[:, task_index * 3: task_index * 3 + 3] = this_jacobian
                 jacobian = sp.simplify(jacobian)
-                jacobians_dict[jacobian_configuration] = (self.Ts, jacobian)
+                calculated_configuration = (Ts, jacobian)
                 with open(jacobians_filename, 'wb') as handle:
-                    pickle.dump(jacobians_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                    pickle.dump(calculated_configuration, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
             else:
                 print("Configuration not defined")
-
-        return sp.lambdify(self.q, jacobian)
+                exit(1)
+        lamb_jacobian = sp.lambdify(self.q, jacobian)
+        lamb_Ts = [sp.lambdify(self.q, T) for T in Ts]
+        return lamb_jacobian, lamb_Ts
 
     def getJacobian(self):
-        # self.printTransformation()
         joint_positions = []
         for joint_handle in self.joint_handles:
             _, joint_position = vrep.simxGetJointPosition(self.clientID, joint_handle, vrep.simx_opmode_buffer)
             joint_positions.append(joint_position)
 
-            self.printTransformation()
-        result = self.f(*joint_positions)
+        self.printTransformation(joint_positions=joint_positions)
+        result = self.jacobian(*joint_positions)
         return result
 
-    def printTransformation(self):
-        joint_positions = []
-        for joint_handle in self.joint_handles:
-            _, joint_position = vrep.simxGetJointPosition(self.clientID, joint_handle, vrep.simx_opmode_buffer)
-            joint_positions.append(joint_position)
-        print(self.Ts[0].subs(zip(self.q, joint_positions))[0:3, 3])
+    def printTransformation(self, joint_positions=None):
+        if joint_positions is None:
+            joint_positions = []
+            for joint_handle in self.joint_handles:
+                _, joint_position = vrep.simxGetJointPosition(self.clientID, joint_handle, vrep.simx_opmode_buffer)
+                joint_positions.append(joint_position)
+        print(self.Ts[0](*joint_positions)[0:3, 3])
