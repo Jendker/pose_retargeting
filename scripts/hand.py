eda3aef4592834fcb4eb0b50fd6ab572aafc68d1
Jedrzej Beniamin Orbik <Jendker@users.noreply.github.com>
2019-04-11 19:22:22 +0200

    Adjustments to points for transform calculation

--- scripts/hand.py
+++ scripts/hand.py
@@ -173,7 +173,8 @@ class HandPart:
         for index in self.task_descriptor_equivalent_hpe_indices:
             hand_part_poses.append(new_data.joints_position[index])
         HPE_hand_part_poses = np.concatenate(hand_part_poses)
-        temp_new_HPE_hand_pose = HPE_hand_part_poses * 0.2 + self.last_human_hand_part_pose * 0.8
+        alpha = 0.1
+        temp_new_HPE_hand_pose = HPE_hand_part_poses * alpha + self.last_human_hand_part_pose * (1 - alpha)
         if self.last_callback_time != 0:  # TODO: maybe here we can make better with calculating with first iteration
             self.human_hand_vel = (temp_new_HPE_hand_pose - self.last_human_hand_part_pose) / (
                     current_time - self.last_callback_time)
--- scripts/mapper.py
+++ scripts/mapper.py
@@ -35,9 +35,7 @@ class Mapper:
         self.camera_frame_name = "camera_link"
         self.last_update = time.time()
         self.using_left_hand = rospy.get_param('transformation/left_hand')
-        self.shift_translation = np.array([-1.5, 0., 1.0])
-
-        self.count_print = 0
+        self.shift_translation = np.array([-0.5, 0., 1.3])
 
         self.simulationFingerLength = 0.096
         self.marker_pub = rospy.Publisher('pose_mapping_vrep/transformed_hand', MarkerArray, queue_size=10)
@@ -107,13 +105,13 @@ class Mapper:
         position = [joint.x, joint.y, joint.z]
         return np.array(position)
 
-    def __publishMarkers(self):
+    def __publishMarkers(self, data):
         message = MarkerArray()
         lines = [[2, 9, 10, 11], [3, 12, 13, 14], [4, 15, 16, 17], [5, 18, 19, 20], [6, 7, 8], [0, 1, 6, 2, 3, 4, 5, 0]]
         time_now = rospy.Time.now()
         for index, line_points in enumerate(lines):
             line_marker = Marker()
-            line_marker.header.frame_id = self.node_frame_name
+            line_marker.header.frame_id = self.camera_frame_name
             line_marker.header.stamp = time_now
             line_marker.id = index
             line_marker.color.r = 1.0
@@ -122,7 +120,7 @@ class Mapper:
             line_marker.scale.x = line_marker.scale.y = line_marker.scale.z = 0.01
             line_marker.type = line_marker.LINE_STRIP
 
-            finger_points = [self.last_data.joints_position[x] for x in line_points]
+            finger_points = [data.joints_position[x] for x in line_points]
 
             for point in finger_points:
                 message_point = Point()
@@ -182,7 +180,10 @@ class Mapper:
             world_points.append(self.__getPositionVectorForDataIndex(data, index))
 
         vrep_points = [np.array([0.033, -.0099, 0.352]), np.array([0.011, -0.0099, .356]), np.array([-.011, -.0099, .352]),
-                       np.array([-0.033, -.0099, .3436]), np.array([-0.01, -0.0145, 0.27]), np.array([0.03, -0.0145, 0.27])]
+                       np.array([-0.033, -.0099, .3436]), np.array([-0.011, -0.005, 0.281]), np.array([0.03, -0.005, 0.285])]
+
+        # print(-math.sqrt(0.071 ** 2 - (0) ** 2 - ((0.0099 - 0.005) ** 2)) + .352)
+        # print(np.linalg.norm(np.array([-.011, -.0099, .352]) - np.array([-0.011, -0.005, 0.281])))
 
         transformation_matrix, correct_rotation_matrix = self.__correspondancePointsTransformation(np.array(world_points), np.array(vrep_points))
 
@@ -224,20 +225,10 @@ class Mapper:
         translation = transformation_matrix[0:3, 3]
         inverse_translation = -np.dot(inverse_rotation_matrix, translation)
         inverse_transformation_matrix = self.__euclideanTransformation(inverse_rotation_matrix, inverse_translation)
-        if self.count_print > 10:
-            copy_inverse_transformation_matrix = inverse_transformation_matrix.copy()
-            scale, shear, angles, trans, persp = tf_conversions.transformations.decompose_matrix(copy_inverse_transformation_matrix)
-            print(scale, "scale")
-            print(np.array(angles) * 180.0 / np.pi)
-            euler = tf_conversions.transformations.euler_from_matrix(copy_inverse_transformation_matrix, 'sxyz')
-            rospy.loginfo("%f %f %f sxyz", euler[0] * 180.0 / np.pi, euler[1] * 180.0 / np.pi, euler[2] * 180.0 / np.pi)
-            euler = tf_conversions.transformations.euler_from_matrix(copy_inverse_transformation_matrix, 'rxyz')
-            rospy.loginfo("%f %f %f rxyz", euler[0] * 180.0 / np.pi, euler[1] * 180.0 / np.pi, euler[2] * 180.0 / np.pi)
-            self.count_print = 0
-        self.count_print += 1
         q = tf_conversions.transformations.quaternion_from_matrix(inverse_transformation_matrix)
         whole_translation = inverse_translation + self.shift_translation  # shift to keep hand above surface
         vrep.simxSetObjectPosition(self.clientID, self.hand_base_handle, -1, whole_translation.tolist(), vrep.simx_opmode_oneshot)
+        q = (np.array(q) / np.linalg.norm(q)).tolist()
         vrep.simxSetObjectQuaternion(self.clientID, self.hand_base_handle, -1, q, vrep.simx_opmode_oneshot)
         # _, dummy_handle = vrep.simxCreateDummy(self.clientID, 0.005, [255, 255, 255, 255], vrep.simx_opmode_blocking)
         # vrep.simxSetObjectPosition(self.clientID, dummy_handle, -1, whole_translation.tolist(), vrep.simx_opmode_oneshot)
@@ -260,20 +251,23 @@ class Mapper:
         return_points.header.frame_id = target_frame
         return return_points
 
+    def pointToNpArray(self, point):
+        return np.array([point.x, point.y, point.z])
+
     def callback(self, data):
         if self.using_left_hand:
             data = self.__mirrorData(data)
         data = self.__transformToCameraLink(data)  # comment to keep hand in 0 position
         transformation_matrix = self.__publishTransformation(data)
         data = self.__transformDataWithTransform(data, transformation_matrix)
+        # self.__publishMarkers(data)
+        # self.publishNewPointCloud(data)
         data.joints_position = self.scaler.scalePoints(data.joints_position)  # ready to save after scaling
         inverse_transformation_matrix = self.__setHandPosition(transformation_matrix)  # comment to keep hand in 0 position
         data = self.__returnTransformation(data, inverse_transformation_matrix)  # comment to keep hand in 0 position
-        self.publishNewPointCloud(data)  # comment to keep hand in 0 position
         self.last_data = data
 
         self.hand.newPositionFromHPE(self.last_data)
-        self.__publishMarkers()
 
     def __executeInverseOnce(self):
         self.hand.controlOnce()
