cd28660927ea188d103966ebe601a69fa262aacb
Jedrzej Beniamin Orbik <Jendker@users.noreply.github.com>
2019-07-14 15:40:23 +0200

    Fix to list and env_name

--- pose_retargeting/joint_handles_dict.py
+++ pose_retargeting/joint_handles_dict.py
@@ -12,8 +12,8 @@ class JointHandlesDict:
                               'RMCP_side_joint', 'RMCP_front_joint', 'RPIP_joint', 'RDIP_joint', 'RTIP_tip',
                               'metacarpal_joint', 'PMCP_side_joint', 'PMCP_front_joint', 'PPIP_joint',
                               'PDIP_joint', 'PTIP_tip', 'TMCP_rotation_joint', 'TMCP_front_joint', 'TPIP_side_joint',
-                              'TPIP_front_joint', 'TDIP_joint', 'TTIP_tip', 'ShadowRobot_base_target',
-                              'ShadowRobot_base_tip']
+                              'TPIP_front_joint', 'TDIP_joint', 'TTIP_tip', 'ShadowRobot_base_tip',
+                              'ShadowRobot_base_target']
         if simulator.type == SimulatorType.VREP:
             self.body_handles_dict = {}
             for handle_name in body_handle_names:
@@ -24,7 +24,7 @@ class JointHandlesDict:
                 self.body_handles_dict[handle_name] = handle
 
         elif simulator.type == SimulatorType.MUJOCO:
-            if simulator.name == 'relocateSR-v0':
+            if simulator.env_name == 'relocateSR-v0':
                 joint_names = ['rh_FFJ4', 'rh_FFJ3', 'rh_FFJ2', 'rh_FFJ1', 'rh_fftip', 'rh_MFJ4', 'rh_MFJ3', 'rh_MFJ2',
                                'rh_MFJ1', 'rh_mftip', 'rh_RFJ4', 'rh_RFJ3', 'rh_RFJ2', 'rh_RFJ1', 'rh_rftip', 'rh_LFJ5',
                                'rh_LFJ4', 'rh_LFJ3', 'rh_LFJ2', 'rh_LFJ1', 'rh_lftip', 'rh_THJ5', 'rh_THJ4', 'rh_THJ3',
@@ -34,7 +34,7 @@ class JointHandlesDict:
                               'rh_rfmiddle', 'rh_rfdistal', 'rh_rftip', 'rh_lfmetacarpal', 'rh_lfknuckle', 'rh_lfproximal',
                               'rh_lfmiddle', 'rh_lfdistal', 'rh_lftip', 'rh_thbase', 'rh_thproximal', 'rh_thhub',
                               'rh_thmiddle', 'rh_thdistal', 'rh_thtip', 'rh_forearm']
-            elif simulator.name == 'relocate-v0':
+            elif simulator.env_name == 'relocate-v0':
                 joint_names = ['FFJ3', 'FFJ2', 'FFJ1', 'FFJ0', 'fftip', 'MFJ3', 'MFJ2', 'MFJ1',
                                'MFJ0', 'mftip', 'RFJ3', 'RFJ2', 'RFJ1', 'RFJ0', 'rftip', 'LFJ4',
                                'LFJ3', 'LFJ2', 'LFJ1', 'LFJ0', 'lftip', 'THJ4', 'THJ3', 'THJ2',
